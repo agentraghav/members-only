80227e254fe55199d6fb2eaba77834062fb03cb2
agentraghav <raghvendrarathore2@gmail.com>
2021-02-18 11:36:32 +0530

    login and register view

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,12 @@ const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
-exports.userRegister = (req, res, next) => {
+exports.userRegisterGet = (req, res, next) => {
   res.render('register');
 };
 
-exports.userLogIn = (req, res, next) => {
+exports.userRegisterPost = (req, res, next) => {};
+
+exports.userLogInGet = (req, res, next) => {
   res.render('login');
 };
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 const user_controller = require('../controllers/userController');
 
-router.get('/login', user_controller.userLogIn);
+router.get('/login', user_controller.userLogInGet);
+
+router.get('/register', user_controller.userRegisterGet);
 
 module.exports = router;
