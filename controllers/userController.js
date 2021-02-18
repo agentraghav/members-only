f96844a8be81fca0c236f49d18a14a230e7ff9ca
agentraghav <raghvendrarathore2@gmail.com>
2021-02-18 11:57:18 +0530

    register post controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,40 @@
 const { validationResult } = require('express-validator');
-
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const { passport } = require('passport');
 
 exports.userRegisterGet = (req, res, next) => {
   res.render('register');
 };
 
-exports.userRegisterPost = (req, res, next) => {};
+exports.userRegisterPost = (req, res, next) => {
+  const errors = validationResult(req);
+  const { password, username, name } = req.body;
+  if (!errors.isEmpty()) {
+    res.render('register', { errors: errors.array() });
+  } else {
+    bcrypt.hash(password, 10, (err, hashedPass) => {
+      if (err) {
+        res.redirect('/');
+      } else {
+        const user = new User({
+          username: username,
+          name: name,
+          password,
+          hashedPass,
+          membership: false,
+        }).save((err) => {
+          if (err) {
+            return next(err);
+          }
+          passport.authenticate('local')(req, res, () => {
+            return res.redirect('/');
+          });
+        });
+      }
+    });
+  }
+};
 
 exports.userLogInGet = (req, res, next) => {
   res.render('login');
---
+++ validators/user-register.js
@@ -0,0 +1,43 @@
+const { body } = require('express-validator');
+
+exports.register_validation = (req, res, next) => [
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Enter a name.')
+    .isAlphanumeric()
+    .withMessage('Enter only alphanumeric charaters on the name')
+    .escape(),
+  body('username')
+    .trim()
+    .isEmail()
+    .withMessage('The username must be an Email')
+    .custom(async (value) => {
+      const userCheck = await User.findOne({ username: value });
+      if (userCheck !== null) {
+        return Promise.reject();
+      }
+      return Promise.resolve();
+    })
+    .withMessage('Username already in use.')
+    .escape(),
+  body('password')
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage('The password must have at least 6 characters.')
+    .isAlphanumeric()
+    .withMessage('Enter only alphanumeric characters on the password.')
+    .escape(),
+  body('confirmPassword')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Enter a confirm password.')
+    .escape()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Password confirmation is incorrect');
+      } else {
+        return true;
+      }
+    }),
+];
