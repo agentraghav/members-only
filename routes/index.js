6199afaf957d7a14657f5e087c88784562791bf6
agentraghav <raghvendrarathore2@gmail.com>
2021-02-19 11:13:44 +0530

    setup the router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('../passport-config');
-const LocalStrategy = require('passport-local').Strategy;
 const user_controller = require('../controllers/userController');
 const user_validation = require('../validators/user-register');
-
+const post_controller = require('../controllers/postController');
+const post_validation = require('../validators/post-create');
 router.get('/register', user_controller.userRegisterGet);
 
 router.post(
@@ -16,5 +15,21 @@ router.post(
 router.get('/login', user_controller.userLogInGet);
 
 router.post('/login', user_controller.userLogInPost);
+
+router.get('/join', authenticated, user_controller.join_get);
+router.post(
+  '/join',
+  authenticated,
+  user_validation.joinValidation,
+  user_controller.join_post
+);
+
+router.get('/create-message', authenticated, post_controller.create_get);
+router.post(
+  '/create-message',
+  authenticated,
+  post_validation.post_validation,
+  post_controller.create_post
+);
 
 module.exports = router;
